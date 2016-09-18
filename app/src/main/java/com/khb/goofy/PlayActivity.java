67bbb56d56875dd0352f9be5021f889777a3d930
Melvin <melvin0008@gmail.com>
2016-09-18 04:32:10 -0400

    merge master and backup

--- app/src/main/java/com/khb/goofy/PlayActivity.java
+++ app/src/main/java/com/khb/goofy/PlayActivity.java
@@ -1,17 +1,23 @@
 package com.khb.goofy;
 
 import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
+import android.graphics.Bitmap;
 import android.media.MediaPlayer;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 
 import com.github.nkzawa.emitter.Emitter;
 import com.github.nkzawa.socketio.client.Socket;
+import com.google.gson.Gson;
 import com.spotify.sdk.android.authentication.AuthenticationClient;
 import com.spotify.sdk.android.authentication.AuthenticationRequest;
 import com.spotify.sdk.android.authentication.AuthenticationResponse;
@@ -25,13 +31,27 @@ import com.spotify.sdk.android.player.Spotify;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.io.BufferedReader;
+import java.io.ByteArrayOutputStream;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+
+
 public class PlayActivity extends Activity implements
         PlayerNotificationCallback, ConnectionStateCallback {
 
     private static final String CLIENT_ID = "a083380c45ed40eb9a13b8856f431fba";
     private static final String REDIRECT_URI = "http://localhost:8888/callback";
     private static final int REQUEST_CODE = 1337;
-
+    private Button mCameraButton;
+    static final int REQUEST_IMAGE_CAPTURE = 1;
+    static StringBuffer emotion = new StringBuffer("happiness");
     private Socket mSocket;
     Button mPlayBtn;
     Button mStopBtn;
@@ -51,6 +71,7 @@ public class PlayActivity extends Activity implements
         mSocket = socket.getSocket();
         mSocket.on(Socket.EVENT_CONNECT, onConnect);
         mSocket.on(Socket.EVENT_DISCONNECT,onDisconnect);
+        mSocket.on(Socket.EVENT_CONNECT_ERROR, onConnectError);
         mSocket.on("track", onTrack);
         mSocket.on("ping",onPing);
         mSocket.on("ponged_reply",onPongedReply);
@@ -64,7 +85,7 @@ public class PlayActivity extends Activity implements
             public void onClick(View v) {
                 JSONObject sendData = new JSONObject();
                 try {
-                    sendData.put("mood","Happy");
+                    sendData.put("mood",emotion.toString());
                 } catch (JSONException e) {
                     e.printStackTrace();
                 }
@@ -75,6 +96,16 @@ public class PlayActivity extends Activity implements
             @Override
             public void onClick(View view) {
                 mSocket.emit("stop", "stop");
+            }
+        });
+        mCameraButton = (Button)findViewById(R.id.camera_button);
+        mCameraButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+                    startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
+                }
             }
         });
 
@@ -109,8 +140,20 @@ public class PlayActivity extends Activity implements
                 @Override
                 public void run() {
                     isConnected = false;
-                    mMediaPlayer.release();
+                    if(mMediaPlayer != null) {
+                        mMediaPlayer.release();
+                    }
                     mlatency = 0;
+                    if (mSocket != null){
+                        mSocket.disconnect();
+                        mSocket.off(Socket.EVENT_CONNECT, onConnect);
+                        mSocket.off(Socket.EVENT_DISCONNECT, onDisconnect);
+                        mSocket.off(Socket.EVENT_CONNECT_ERROR, onConnectError);
+                        mSocket.off("track", onTrack);
+                        mSocket.off("ping",onPing);
+                        mSocket.off("ponged_reply",onPongedReply);
+                        mSocket.off("stop_track", onStopTrack);
+                    }
                 }
             });
         }
@@ -239,6 +282,27 @@ public class PlayActivity extends Activity implements
         }
     };
 
+    private Emitter.Listener onConnectError = new Emitter.Listener() {
+        @Override
+        public void call(Object... args) {
+            runOnUiThread(new Runnable() {
+                @Override
+                public void run() {
+                    if (mSocket != null){
+                        mSocket.disconnect();
+                        mSocket.off(Socket.EVENT_CONNECT, onConnect);
+                        mSocket.off(Socket.EVENT_DISCONNECT, onDisconnect);
+                        mSocket.off(Socket.EVENT_CONNECT_ERROR, onConnectError);
+                        mSocket.off("track", onTrack);
+                        mSocket.off("ping",onPing);
+                        mSocket.off("ponged_reply",onPongedReply);
+                        mSocket.off("stop_track", onStopTrack);
+                    }
+                }
+            });
+        }
+    };
+
     @Override
     public void onDestroy() {
         super.onDestroy();
@@ -247,6 +311,7 @@ public class PlayActivity extends Activity implements
 
         mSocket.off(Socket.EVENT_CONNECT, onConnect);
         mSocket.off(Socket.EVENT_DISCONNECT, onDisconnect);
+        mSocket.off(Socket.EVENT_CONNECT_ERROR, onConnectError);
         mSocket.off("track", onTrack);
         mSocket.off("ping",onPing);
         mSocket.off("ponged_reply",onPongedReply);
@@ -263,6 +328,18 @@ public class PlayActivity extends Activity implements
 
             }
         }
+        else if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
+            Bundle extras = data.getExtras();
+            Bitmap imageBitmap = (Bitmap) extras.get("data");
+            ImageView imgView = (ImageView)findViewById(R.id.imageView);
+            imgView.setImageBitmap(imageBitmap);
+            sendImage(imageBitmap);
+        }
+    }
+    private void sendImage(Bitmap image) {
+
+        GetImageStatistics stat = new GetImageStatistics(getApplicationContext(), mSocket,emotion);
+        stat.execute(image);
     }
 
     @Override
@@ -320,5 +397,97 @@ public class PlayActivity extends Activity implements
     @Override
     public void onPlaybackError(ErrorType errorType, String s) {
 
+    }
+}
+
+
+class GetImageStatistics extends AsyncTask<Bitmap, Void , String> {
+    protected Context mContext;
+    protected Socket mSocket;
+    String mEmotion = null;
+    StringBuffer mBuff = null;
+    GetImageStatistics(Context context, Socket socket , StringBuffer buff) {
+        this.mContext = context;
+        this.mSocket = socket;
+        this.mBuff = buff;
+    }
+    @Override
+    protected String doInBackground(Bitmap... Params) {
+
+        try {
+            URL url = new URL("https://api.projectoxford.ai/emotion/v1.0/recognize");
+            HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+            connection.setUseCaches (false);
+            connection.setDoInput(true);
+            connection.setDoOutput(true);
+            connection.setRequestMethod("POST");
+            connection.setRequestProperty("content-type", "application/octet-stream");
+            connection.setRequestProperty("Ocp-Apim-Subscription-Key", "b943675cbe41441a90e2b42d2abecf38");
+            connection.connect();
+            ByteArrayOutputStream stream = new ByteArrayOutputStream();
+            Params[0].compress(Bitmap.CompressFormat.PNG, 100, stream);
+            byte[] byteArray = stream.toByteArray();
+
+            OutputStream os = connection.getOutputStream();
+            os.write(byteArray);
+            os.flush();
+            os.close();
+
+            Log.d("Response code is", Integer.toString(connection.getResponseCode()));
+            InputStream is = connection.getInputStream();
+            BufferedReader rd = new BufferedReader(new InputStreamReader(is));
+
+            StringBuffer response = new StringBuffer();
+            setEmotionObject(rd, response);
+
+            Log.d("Response", response.toString());
+        } catch (Exception e) {
+            Log.d("Error", e.toString());
+        }
+        return mEmotion;
+    }
+    @Override
+    protected void onPostExecute(String emotion) {
+        //emotion
+        JSONObject sendData = new JSONObject();
+        try {
+            sendData.put("mood",mEmotion);
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+        mSocket.emit("mood",sendData);
+        mBuff.delete(0, mBuff.length());
+        mBuff.append(mEmotion);
+    }
+    private  void setEmotionObject(BufferedReader rd, StringBuffer response) {
+        String line;
+
+
+        try {
+            while ((line = rd.readLine()) != null) {
+                response.append(line);
+                response.append('\n');
+            }
+            ArrayList<Object> one= new Gson().fromJson(response.toString(), ArrayList.class);
+            Map<String, Object> jsonJavaRootObject = (Map<String, Object>) one.get(0);
+            Map<String, Double> emotionMap = (Map)jsonJavaRootObject.get("scores");
+            Map.Entry<String, Double> maxEntry = null;
+            Map<String, Double> myMap = new HashMap<String, Double>();
+            myMap.put("anger", emotionMap.get("anger"));
+            myMap.put("sadness", emotionMap.get("sadness"));
+            myMap.put("happiness", emotionMap.get("happiness"));
+            for (Map.Entry<String, Double> entry : myMap.entrySet())
+            {
+                if (maxEntry == null || entry.getValue().compareTo(maxEntry.getValue()) > 0)
+                    maxEntry = entry;
+            }
+            Log.d("Anger", maxEntry.toString().split("=")[0]);
+            mEmotion = maxEntry.toString().split("=")[0];
+
+        } catch (Exception e) {
+            Log.d("Error", e.toString());
+            return;
+        }
+        return;
     }
 }
